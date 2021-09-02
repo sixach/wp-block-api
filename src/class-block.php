4c624a39348f534803c3580e104c769029c74fcf
Markus <markus.goeckeritz@sixa.ch>
2021-09-02 20:06:30 +0200

    added repository files and configs

---
+++ src/class-block.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Sixa_Blocks;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+abstract class Block {
+
+    /**
+     * Record if the block was initialized to make sure it is
+     * initialized at most once.
+     *
+     * @since    1.0.0
+     * @var      bool
+     */
+    private static $was_initialized = false;
+
+    /**
+     * Initialize the block.
+     * Set up the WordPress hook to register the block.
+     *
+     * @since     1.0.0
+     * @return    void
+     */
+    public static function init(): void {
+        // Bail early if the block was already initialized.
+        if ( self::$was_initialized ) {
+            return;
+        }
+
+        add_action( 'init', array( __CLASS__, 'register' ) );
+    }
+
+    /**
+     * Registers the block using the metadata loaded from the `block.json` file.
+     * Behind the scenes, it registers also all assets so they can be enqueued
+     * through the block editor in the corresponding context.
+     *
+     * @see       https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+     * @since     1.0.0
+     * @return    void
+     */
+    private static function register(): void {
+        register_block_type_from_metadata( dirname( __DIR__ ) );
+    }
+
+}
