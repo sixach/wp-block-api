0d5d31559ba798d9f0e92e52c297a54e07884bf3
Markus <markus.goeckeritz@sixa.ch>
2021-09-03 16:51:29 +0200

    added is_woocommerce function

--- src/class-woocommerce-block.php
+++ src/class-woocommerce-block.php
@@ -39,11 +39,35 @@ if ( ! class_exists( WooCommerce_Block::class ) ) :
 		 */
 		public static function init(): void {
 			// Bail early if WooCommerce is not activated
-			if ( ! function_exists( 'is_woocommerce' ) || ! is_woocommerce() ) {
+			if ( ! static::is_woocommerce() ) {
 				return;
 			}
 
 			parent::init();
+		}
+
+		/**
+		 * Query WooCommerce activation.
+		 *
+		 * @since   1.0.0
+		 * @return  bool
+		 */
+		private static function is_woocommerce(): bool {
+			// This statement prevents from producing fatal errors,
+			// in case the WooCommerce plugin is not activated on the site.
+			$woocommerce_plugin = apply_filters( 'sixa_add_to_cart_block_woocommerce_path', 'woocommerce/woocommerce.php' );
+			// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+			$subsite_active_plugins = apply_filters( 'active_plugins', get_option( 'active_plugins' ) );
+			// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound
+			$network_active_plugins = apply_filters( 'active_plugins', get_site_option( 'active_sitewide_plugins' ) );
+
+			// Bail early in case the plugin is not activated on the website.
+			// phpcs:ignore WordPress.PHP.StrictInArray.MissingTrueStrict
+			if ( ( empty( $subsite_active_plugins ) || ! in_array( $woocommerce_plugin, $subsite_active_plugins ) ) && ( empty( $network_active_plugins ) || ! array_key_exists( $woocommerce_plugin, $network_active_plugins ) ) ) {
+				return false;
+			}
+
+			return true;
 		}
 
 	}
