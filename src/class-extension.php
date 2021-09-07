e77c22dcf79b06bdcf9838327435a4c18c67cdf6
Markus <markus.goeckeritz@sixa.ch>
2021-09-07 23:52:47 +0200

    added comments

--- src/class-extension.php
+++ src/class-extension.php
@@ -6,6 +6,7 @@ namespace Sixa_Blocks;
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
 if ( ! class_exists( Extension::class ) ) :
 
 	class Extension {
@@ -44,39 +45,73 @@ if ( ! class_exists( Extension::class ) ) :
 		}
 
 		/**
-		 * @return string
+		 * Return the full absolute path of the build directory in an extension.
+		 * This is the full absolute path to the root directory of the extension with the
+		 * name of the build directory appended to it.
+		 *
+		 * Notice that this function assumes a certain structure of your extension package.
+		 * Particularly, this function assumes that the directory that contains the PHP
+		 * extension class is in a directory in the root directory of your extension.
+		 *
+		 * @since     1.0.0
+		 * @param     string    $build_dir    Name of the build directory.
+		 * @return    string
 		 */
 		public static function get_build_dir_path( $build_dir = API_Constants::BUILD_DIR ): string {
 			return trailingslashit( sprintf( '%s%s', trailingslashit( dirname( static::$path, 2 ) ), $build_dir ) );
 		}
 
 		/**
-		 * @param $file_name
-		 * @return string
+		 * Return the full absolute path of the passed build file in an extension.
+		 * This is the full absolute path to the root directory of the extension with the
+		 * build directory and file name attached. The passed file must be inside the build
+		 * directory.
+		 *
+		 * @since     1.0.0
+		 * @param     string    $file_name    Name of a file (including extension).
+		 * @param     string    $build_dir    Name of the build directory.
+		 * @return    string
 		 */
 		public static function get_build_file_path( $file_name, $build_dir = API_Constants::BUILD_DIR ) {
 			return sprintf( '%s%s', static::get_build_dir_path( $build_dir ), $file_name );
 		}
 
 		/**
-		 * @param $file_name
-		 * @return string
+		 * Return the URL file path for the passed build file in an extension.
+		 *
+		 * @since     1.0.0
+		 * @param     string    $file_name    Name of a file (including extension).
+		 * @param     string    $build_dir    Name of the build directory.
+		 * @return    string
 		 */
 		public static function get_build_file_url( $file_name, $build_dir = API_Constants::BUILD_DIR ) {
 			return sprintf( '%s%s%s', plugin_dir_url( dirname( static::$path ) ), trailingslashit( $build_dir ), $file_name );
 		}
 
 		/**
-		 * @param $file_name
-		 * @return string
+		 * Return the full absolute path for a PHP asset file for a given build file
+		 * in an extension.
+		 *
+		 * @since     1.0.0
+		 * @param     string    $file_name    Name of a file (including extension).
+		 * @param     string    $build_dir    Name of the build directory.
+		 * @return    string
 		 */
 		public static function get_build_asset_path( $file_name, $build_dir = API_Constants::BUILD_DIR ) {
 			return static::get_build_file_path( static::get_asset_file_name( $file_name ), $build_dir );
 		}
 
 		/**
-		 * @param $build_file_name
-		 * @return string
+		 * Build and return the asset file name from a build file name.
+		 *
+		 * The asset file is typically auto-generated and follows
+		 * a naming pattern relative to the original file name. For instance,
+		 * index.asset.php for index.js. This function returns the asset file
+		 * name following this pattern.
+		 *
+		 * @since     1.0.0
+		 * @param     string    $build_file_name    Name of the original file (e.g. index.js).
+		 * @return    string
 		 */
 		public static function get_asset_file_name( $build_file_name ) {
 			$file_name_parts = explode( '.', $build_file_name );
@@ -111,6 +146,17 @@ if ( ! class_exists( Extension::class ) ) :
 			print( 'enqueue_frontend_assets <br><br>' );
 		}
 
+		/**
+		 * Enqueue the passed script.
+		 *
+		 * In addition to simply passing the given file to the WordPress
+		 * enqueue function, this function builds and extracts all
+		 * relevant paths, names, and parameters.
+		 *
+		 * @since     1.0.0
+		 * @param     $file_name    Name of the build file to be enqueued.
+		 * @return    void
+		 */
 		public static function enqueue_script( $file_name ) {
 			$asset_path = static::get_build_file_url( $file_name );
 			$asset_dependencies = static::get_asset_dependencies( $file_name );
@@ -124,6 +170,17 @@ if ( ! class_exists( Extension::class ) ) :
 			);
 		}
 
+		/**
+		 * Enqueue the passed stylesheet.
+		 *
+		 * In addition to simply passing the given file to the WordPress
+		 * enqueue function, this function builds and extracts all
+		 * relevant paths, names, and parameters.
+		 *
+		 * @since     1.0.0
+		 * @param     $file_name    Name of the build file to be enqueued.
+		 * @return    void
+		 */
 		public static function enqueue_style( $file_name ) {
 			$asset_path = static::get_build_file_url( $file_name );
 			$asset_dependencies = static::get_asset_dependencies( $file_name );
@@ -137,6 +194,16 @@ if ( ! class_exists( Extension::class ) ) :
 			);
 		}
 
+		/**
+		 * Return asset dependencies and asset version for the
+		 * given build file. If available, this information is extracted
+		 * from an auto-generated PHP asset file. Otherwise, this function
+		 * returns a set of fallback values.
+		 *
+		 * @since     1.0.0
+		 * @param     $file_name    Name of the build file to be enqueued.
+		 * @return    array
+		 */
 		public static function get_asset_dependencies( $file_name ) {
 			$file_path = static::get_build_file_path( $file_name );
 			$asset_path = static::get_build_asset_path( $file_name );
