a437b1f081371a0c2c2203e4705ce39d9ff11ce5
Markus <markus.goeckeritz@sixa.ch>
2021-09-07 23:25:48 +0200

    added comments and abspath + class_exists checks

--- src/class-api-constants.php
+++ src/class-api-constants.php
@@ -1,9 +1,37 @@
 <?php
+/**
+ * Constants for block and extension class library.
+ *
+ * @link      https://sixa.ch
+ * @author    sixa AG
+ * @since     1.0.0
+ *
+ * @package       Sixa_Blocks
+ * @subpackage    Sixa_Blocks/API_Constants
+ */
 
 namespace Sixa_Blocks;
 
-class API_Constants {
-
-	public const BUILD_DIR = 'build';
-
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
+
+if ( ! class_exists( API_Constants::class ) ) :
+
+	/**
+	 * Constants used throughout this library.
+	 */
+	class API_Constants {
+
+		/**
+		 * Name of the directory inside an extension that contains the build assets.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const BUILD_DIR = 'build';
+
+	}
+
+endif;
