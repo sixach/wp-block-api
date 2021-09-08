f7c56b31c3494806a57fa44e41e8959f2f35b996
Markus <markus.goeckeritz@sixa.ch>
2021-09-08 11:57:38 +0200

    updated file comment

--- src/interface-extension-with-block-assets.php
+++ src/interface-extension-with-block-assets.php
@@ -1,4 +1,34 @@
 <?php
+/**
+ * Interface for Extensions with block assets.
+ *
+ * Use this interface if your extension loads block assets and add the assets
+ * your extension should load in your implementation of `enqueue_block_assets`.
+ *
+ * `Extension` automatically adds `enqueue_block_assets` to the WordPress action
+ * hook `enqueue_block_assets` if the extending class implements this interface.
+ * That is, you need not add the hook yourself.
+ *
+ * Note that there are additional helper functions available in `Extension` to
+ * simplify the registration of assets. These helper functions are
+ * `Extension::enqueue_style` and `Extension::enqueue_script`.
+ *
+ * The default implementation of `enqueue_block_assets` could look as follows:
+ *
+ * ```PHP
+ * class My_Extension extends Extension implements Extension_With_Block_Assets {
+ *
+ *     protected static string $name = 'sixa-my-extension';
+ *
+ *     public static function enqueue_block_assets(): void {
+ *         self::enqueue_script( 'style-index.css' );
+ *     }
+ *
+ * }
+ * ```
+ *
+ * Block assets are loaded in the editor as well as the frontend.
+ */
 
 namespace Sixa_Blocks;
 
