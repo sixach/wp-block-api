5816f24c421b56d108383a5cbdaf22e9661bc6ef
Markus <markus.goeckeritz@sixa.ch>
2021-09-09 12:06:26 +0200

    added asset types, testing handle generation

---
+++ src/class-asset-type.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Constants for asset types.
+ *
+ * We introduce these asset types for the extension JSON definition. However, the naming and
+ * usages of these asset types follows the same definitions that WordPress uses for blocks.
+ * That is, we apply the same naming such that the prefix indicates where the assets are loaded
+ * and no prefix indicates that the assets are loaded in the editor and the frontend.
+ *
+ *     `style` -> editor + frontend
+ *     `script` -> editor + frontend
+ *     `editorStyle` -> editor
+ *     `editorScript` -> editor
+ *     `frontendStyle` -> frontend
+ *     `frontendScript` -> frontend
+ *
+ * @link      https://sixa.ch
+ * @author    sixa AG
+ * @since     1.0.0
+ *
+ * @package       Sixa_Blocks
+ * @subpackage    Sixa_Blocks/Asset_Types
+ */
+
+namespace Sixa_Blocks;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( Asset_Types::class ) ) :
+
+	/**
+	 * Constants used throughout this library.
+	 */
+	class Asset_Type {
+
+		/**
+		 * Name of the style asset that is enqueued in the editor and the frontend.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const STYLE = 'style';
+
+		/**
+		 * Name of the script asset that is enqueued in the editor and the frontend.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const SCRIPT = 'script';
+
+		/**
+		 * Name of the script asset that is enqueued in the editor.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const EDITOR_SCRIPT = 'editorScript';
+
+		/**
+		 * Name of the style asset that is enqueued in the editor.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const EDITOR_STYLE = 'editorStyle';
+
+		/**
+		 * Name of the script asset that is enqueued in the frontend.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const FRONTEND_SCRIPT = 'frontendScript';
+
+		/**
+		 * Name of the script asset that is enqueued in the frontend.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const FRONTEND_STYLE = 'frontendStyle';
+
+		/**
+		 * Mapping of names to slugs.
+		 *
+		 * Names are used in the JSON definition whereas slugs are used in handles.
+		 *
+		 * @var      string
+		 * @since    1.0.0
+		 */
+		public const SLUGS = array(
+			Asset_Types::STYLE => 'style',
+			Asset_Types::SCRIPT => 'script',
+			Asset_Types::EDITOR_SCRIPT => 'editor-script',
+			Asset_Types::EDITOR_STYLE => 'editor-style',
+			Asset_Types::FRONTEND_SCRIPT => 'frontend-script',
+			Asset_Types::FRONTEND_STYLE => 'frontend-style',
+		);
+
+	}
+
+endif;
--- src/class-functions.php
+++ src/class-functions.php
@@ -6,9 +6,11 @@ namespace Sixa_Blocks;
 final class Functions {
 
 	private const METADATA_FILE_NAME = 'extension.json';
+	private const PATH_PREFIX = 'file:';
 
 	public static function register_extension_from_metadata( string $file_or_folder, array $args = array() ): void {
-		$metadata_file = Functions::get_metadata_file_path_from_file_or_folder( $file_or_folder );
+		$metadata_file = self::get_metadata_file_path_from_file_or_folder( $file_or_folder );
+
 
 		if ( ! file_exists( $metadata_file ) ) {
 			// Bail early if the given file does not exist.
@@ -16,17 +18,54 @@ final class Functions {
 		}
 
 		$metadata = json_decode( file_get_contents( $metadata_file ), true );
-		if ( ! is_array( $metadata ) || empty( $metadata ) ) {
-			// Bail early if the extracted object is invalid or empty.
+		if ( ! is_array( $metadata ) || empty( $metadata['name'] ) ) {
+			// Bail early if the extracted object is invalid or if `name` is missing.
 			return;
 		}
+
+		if ( ! empty( $metadata[ Asset_Type::STYLE ] ) ) {
+			self::register_extension_script_handle( $metadata['name'], $metadata['script'], Asset_Type::STYLE );
+		}
+
 	}
 
-	private static function get_metadata_file_path_from_file_or_folder( string $file_or_folder, string $metadata_filename = Functions::METADATA_FILE_NAME ): string {
+	/**
+	 * @param string $file_or_folder
+	 * @param string $metadata_filename
+	 * @return string
+	 */
+	private static function get_metadata_file_path_from_file_or_folder( string $file_or_folder, string $metadata_filename = self::METADATA_FILE_NAME ): string {
 		if ( $metadata_filename !== substr( $file_or_folder, strlen( $metadata_filename ) * -1 ) ) {
 			return sprintf( '%s%s', trailingslashit( $file_or_folder ), $metadata_filename );
 		}
 		return $file_or_folder;
+	}
+
+	private static function register_extension_script_handle( string $name, string $path, string $type ): string {
+		$handle = self::remove_path_prefix( $path );
+		// Bail early if the passed path already is a handle (i.e. if it doesn't contain a 'file:' prefix)
+		if ( $handle === $path ) {
+			return $handle;
+		}
+
+		$handle = self::get_asset_handle( $name, $type );
+		print( $handle );
+		exit();
+	}
+
+	/**
+	 * @param string $path
+	 * @return false|string
+	 */
+	private static function remove_path_prefix( string $path ) {
+		if ( 0 !== strpos( $path, self::PATH_PREFIX ) ) {
+			return $path;
+		}
+		return substr( $path, strlen( self::PATH_PREFIX ) );
+	}
+
+	private static function get_asset_handle( $name, $type ) {
+		return sprintf( '%s-%s', $name, Asset_Type::SLUGS[ $type ] );
 	}
 
 }
