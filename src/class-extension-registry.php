e95a1eb1685e22852073e50e175cc0bbe4259e17
Markus <markus.goeckeritz@sixa.ch>
2021-09-09 19:01:58 +0200

    polished class

--- src/class-extension-registry.php
+++ src/class-extension-registry.php
@@ -1,47 +1,95 @@
 <?php
+/**
+ * Extension Registry.
+ *
+ * This class is used to register extensions and retrieve all registered extensions
+ * subsequently to enqueue extension assets.
+ *
+ * @link          https://sixa.ch
+ * @author        sixa AG
+ * @since         1.0.0
+ *
+ * @package       Sixa_Blocks
+ * @subpackage    Sixa_Blocks/Extension_Registry
+ */
 
 namespace Sixa_Blocks;
 
-final class Extension_Registry {
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-	private $registered_extensions = array();
-
-	private static $instance = null;
-
-	public function register( $extension ) {
-		// TODO: add _doing_it_wrong or WP_Error here
-		$this->registered_extensions[ $extension['name'] ] = $extension;
-	}
+if ( ! class_exists( Extension_Registry::class ) ) :
 
 	/**
-	 * Return all registered extensions.
+	 * Extension_Registry Class.
 	 *
-	 * @since     1.0.0
-	 * @return    array
+	 * Used to register extensions and retrieve registered extensions subsequently
+	 * for further processing (e.g. asset enqueueing).
 	 */
-	public function get_registered_extensions(): array {
-		return $this->registered_extensions;
-	}
+	final class Extension_Registry {
 
-	/**
-	 * Return an instance of this class.
-	 * Either returns the existing instance or creates a new instance.
-	 *
-	 * @since     1.0.0
-	 * @return    Extension_Registry
-	 */
-	public static function get_instance(): Extension_Registry {
-		if ( null === self::$instance ) {
-			// Add actions that perform asset enqueueing.
-			// Inside `Extension_Registry::get_instance()` seems to be a convenient location
-			// for this as i) it is guaranteed to be called if at least one extension is registered
-			// and ii) the condition `null === self::$instance` is guaranteed to be true at most once.
-			// Thus, we can guarantee that we are only adding the actions once (and would in fact be able
-			// to skip the checks that we include).
-			Functions::add_enqueueing_actions();
-			self::$instance = new self();
+		/**
+		 * Map of all registered extensions.
+		 * Holds the data of an extension with its name as the key.
+		 *
+		 * @since    1.0.0
+		 * @var      array
+		 */
+		private array $registered_extensions = array();
+
+		/**
+		 * An instance of this class.
+		 *
+		 * @since    1.0.0
+		 * @var      Extension_Registry|null
+		 */
+		private static ?Extension_Registry $instance = null;
+
+		/**
+		 * Register an extension.
+		 *
+		 * @since     1.0.0
+		 * @param     array    $extension
+		 * @return    void
+		 */
+		public function register( array $extension ): void {
+			// TODO: add _doing_it_wrong or WP_Error here ?
+			$this->registered_extensions[ $extension['name'] ] = $extension;
 		}
 
-		return self::$instance;
+		/**
+		 * Return all registered extensions.
+		 *
+		 * @since     1.0.0
+		 * @return    array
+		 */
+		public function get_registered_extensions(): array {
+			return $this->registered_extensions;
+		}
+
+		/**
+		 * Return an instance of this class.
+		 * Either returns the existing instance or creates a new instance.
+		 *
+		 * @since     1.0.0
+		 * @return    Extension_Registry
+		 */
+		public static function get_instance(): Extension_Registry {
+			if ( null === self::$instance ) {
+				// Add actions that perform asset enqueueing.
+				// Inside `Extension_Registry::get_instance()` seems to be a convenient location
+				// for this as i) it is guaranteed to be called if at least one extension is registered
+				// and ii) the condition `null === self::$instance` is guaranteed to be true at most once.
+				// Thus, we can guarantee that we are only adding the actions once (and would in fact be able
+				// to skip the checks that we include).
+				Functions::add_enqueueing_actions();
+				self::$instance = new self();
+			}
+
+			return self::$instance;
+		}
 	}
-}
+
+endif;
